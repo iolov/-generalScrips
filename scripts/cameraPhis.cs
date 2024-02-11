738cee054c24663f8e99528fac777876377082a3
iolov <102531510+iolov@users.noreply.github.com>
2024-02-11 13:14:46 +0300

    sec

--- scripts/cameraPhis.cs
+++ scripts/cameraPhis.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class cameraPhis : MonoBehaviour
 {
-    [SerializeField] float sens = 100;
+    [SerializeField] float sens;
     [SerializeField] GameObject player;
     float xRot;
 
@@ -14,11 +14,12 @@ public class cameraPhis : MonoBehaviour
     }
     void FixedUpdate()
     {
-        float mouseX = Input.GetAxis("Mouse X") * sens * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * sens * Time.deltaTime;
+        float mouseX = Input.GetAxis("Mouse X") * sens*100* Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * sens*100* Time.deltaTime;
 
         xRot -= mouseY;
         xRot = Mathf.Clamp(xRot, -90, 90);
+        print(xRot);
 
         transform.localRotation = Quaternion.Euler(xRot, 0f, 0f);
         player.transform.Rotate(Vector3.up * mouseX*1.5f);
